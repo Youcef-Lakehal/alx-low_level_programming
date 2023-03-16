14ef446e47d37e660788ad852c208c389e78cf06
Youcef-Lakehal <y.lakehal@xpertsoft-dz.com>
2023-03-16 16:53:12 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints using puts function
+ *
+ *  Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
