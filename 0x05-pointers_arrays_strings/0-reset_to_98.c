79eda283195820d4d0c563905627ba478881f953
Youcef-Lakehal <y.lakehal@xpertsoft-dz.com>
2023-03-27 13:01:00 -0700

    correct task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,18 +1,11 @@
-#include "holberton.h"
-#include <stdio.h>
+#include <main.h>
 
 /**
- * main - check the code.
+ * main - change adress .
  *
  * Return: Always 0.
  */
-int main(void)
+void reset_to_98(int *n)
 {
-    int n;
-
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-    return (0);
+	*n = 98;
 }
