90e9761df4934df57a68f999969cdf27aceab4df
Youcef-Lakehal <y.lakehal@xpertsoft-dz.com>
2023-03-30 18:17:56 -0700

    correction 4 task 0,1,2

--- 0x06-pointers_arrays_strings/0-stract.c
+++ 0x06-pointers_arrays_strings/0-stract.c
@@ -9,16 +9,16 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int c, c2;
+int c, c2;
 
-	c = 0;
+c = 0;
 
-	while (dest[c])
-	{
-		c++;
-	}
+while (dest[c])
+{
+	c++;
+}
 
-	for (c2 = 0; src[c2]; c2++)
-		dest[c++] = src[c2];
-	return (dest);
+for (c2 = 0; src[c2]; c2++)
+	dest[c++] = src[c2];
+return (dest);
 }
