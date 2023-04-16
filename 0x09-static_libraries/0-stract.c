3c27b24149f52264f4f6e8c8adcc3f6cba21c8af
Youcef-Lakehal <y.lakehal@xpertsoft-dz.com>
2023-04-15 17:08:35 -0700

    libmy

---
+++ 0x09-static_libraries/0-stract.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcat - concat 2 string
+ * @dest:char
+ * @src:char
+ * Return:char
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int c, c2;
+
+c = 0;
+
+while (dest[c])
+{
+	c++;
+}
+
+for (c2 = 0; src[c2]; c2++)
+	dest[c++] = src[c2];
+return (dest);
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-this is a Read me file 
